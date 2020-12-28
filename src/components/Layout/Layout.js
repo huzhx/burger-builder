906b4693d26686dccd6d401da64c8135dbd122cc
Zhaoxian Hu <huzhx@uci.edu>
2020-12-28 13:22:15 -0800

    add Layout.module.css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from './Layout.module.css';
+
 const Layout = ({ children }) => {
   return (
     <>
       <div>Toolbar</div>
-      <main>{children}</main>
+      <main className={styles.layout__content}>{children}</main>
     </>
   );
 };
