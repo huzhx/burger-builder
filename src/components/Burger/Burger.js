8418fb78ecaec864fe51260626ad9598a829f64c
Zhaoxian Hu <huzhx@uci.edu>
2020-12-28 14:45:27 -0800

    correct the error in the flat array script

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,22 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = ({ ingredients }) => {
-  const ingredientsArr = Object.keys(ingredients).map((key) => {
-    return [...Array(ingredients[key])]
-      .map((_, i) => <BurgerIngredient key={key + i} type={key} />)
-      .reduce((arr, el) => {
-        return [...arr, el];
-      }, []);
-  });
+  let ingredientsTransformed = Object.keys(ingredients)
+    .map((key) => {
+      return [...Array(ingredients[key])].map((_, i) => <BurgerIngredient key={key + i} type={key} />);
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  console.log({ ingredientsTransformed });
+  if (ingredientsTransformed.length === 0) {
+    console.log('im here');
+    ingredientsTransformed = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={styles.burger}>
       <BurgerIngredient type="bread-top" />
-      {ingredientsArr}
+      {ingredientsTransformed}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
