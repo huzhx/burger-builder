0b3a4cdc1d784cff1db0c50b6a79ea44e406d2c5
Zhaoxian Hu <huzhx@uci.edu>
2020-12-28 21:21:54 -0800

    add add-ingredients handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -6,10 +6,26 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 const BurgerBuilder = () => {
   const [ingredients, setIngredients] = useState({ salad: 1, bacon: 1, cheese: 1 });
 
+  const handleAddIngredients = (type) => {
+    setIngredients((prev) => {
+      const newIngredients = { ...prev };
+      if (type in newIngredients) {
+        newIngredients[type]++;
+      } else {
+        newIngredients[type] = 1;
+      }
+      return newIngredients;
+    });
+  };
+
+  useEffect(() => {
+    console.log({ ingredients });
+  }, [ingredients]);
+
   return (
     <>
       <Burger ingredients={ingredients} />
-      <BuildControls />
+      <BuildControls onAddIngredients={handleAddIngredients} />
     </>
   );
 };
